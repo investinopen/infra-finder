4dddd8b0cced4918ba390db43c3f3230a4f5eab3
jen-castiron <jen@castironcoding.com>
2024-03-29 14:02:16 -0400

    [F] Show back button

--- app/components/share_bar_component.rb
+++ app/components/share_bar_component.rb
@@ -4,8 +4,13 @@ class ShareBarComponent < ApplicationComponent
   # @return [Boolean, nil]
   attr_reader :show_back
 
+  # @return [String, nil]
+  attr_reader :class_name
+
   # @param [Boolean] show_back
-  def initialize(show_back: false)
+  # @param [Boolean] is_narrow
+  def initialize(show_back: false, is_narrow: false)
     @show_back = show_back
+    @class_name = is_narrow ? "l-container--narrow" : ""
   end
 end
