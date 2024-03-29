fd94baa8162c3dedd9301273cabbd97e0191fcd9
Alexa Grey <alexa@castironcoding.com>
2024-03-29 11:32:53 -0700

    [C] Split mobile filters into their own form

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -43,6 +43,47 @@ class ApplicationComponent < ViewComponent::Base
     end
   end
 
+  module GeneratesSolutionSearch
+    extend ActiveSupport::Concern
+
+    # @return [Ransack::Search]
+    attr_reader :solution_search
+
+    # @api private
+    # @return [Hash]
+    attr_reader :search_form_options
+
+    # @param [Ransack::Search] solution_search
+    def initialize(solution_search: Solution.all.ransack({}))
+      @solution_search = solution_search
+    end
+
+    def before_render
+      # :nocov:
+      super if defined?(super)
+      # :nocov:
+
+      @search_form_options = build_search_form_options
+    end
+
+    private
+
+    def build_search_form_options
+      {
+        url: solution_search_path,
+        html: {
+          autocomplete: "off",
+          data: {
+            method: "post",
+            "turbo-frame": "solutions-list",
+            "turbo-method": "post",
+          },
+          method: :post,
+        },
+      }
+    end
+  end
+
   # @api private
   module LoadsCurrentUser
     extend ActiveSupport::Concern
--- app/components/solution_mobile_filters_component.rb
+++ app/components/solution_mobile_filters_component.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 class SolutionMobileFiltersComponent < ApplicationComponent
-  # @return [Ransack::Helpers::FormBuilder] f
-  attr_reader :f
-
-  # @param [Ransack::Helpers::FormBuilder] f
-  def initialize(f:)
-    @f = f
-  end
+  include GeneratesSolutionSearch
 end
