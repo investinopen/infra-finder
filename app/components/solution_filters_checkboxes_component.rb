45d914b7e122a77dd1562aa27f21c0c23cf2b3fd
jen-castiron <jen@castironcoding.com>
2024-03-29 15:01:48 -0400

    [B] Fix mobile filter button styling

--- app/components/solution_filters_checkboxes_component.rb
+++ app/components/solution_filters_checkboxes_component.rb
@@ -8,16 +8,4 @@ class SolutionFiltersCheckboxesComponent < ApplicationComponent
   def initialize(f:)
     @f = f
   end
-
-  # NOTE: Does not work when component is shared between two views
-  # @param [Symbol] scope_name
-  # @return [String]
-  def implementation_checkbox(scope_name)
-    f.label scope_name, class: "m-checkbox" do
-      capture do
-        concat f.check_box scope_name, {}, "true", "false"
-        concat content_tag :span, t(".implementations.#{scope_name}", raise: true)
-      end
-    end
-  end
 end
