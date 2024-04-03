876fdafa026c09a988150c4543ba72a6dc55bb8c
jen-castiron <jen@castironcoding.com>
2024-04-03 11:41:19 -0400

    [F] Change l-nav-bar to m-nav-bar, show active section in detail nav

--- app/components/solution_details_component/solution_details_component_controller.js
+++ app/components/solution_details_component/solution_details_component_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  connect() {
-    // Intentionally left blank
+  static targets = ["article"];
+
+  articleTargetConnected(element) {
+    const observer = new IntersectionObserver(this.intersect, {
+      threshold: [0.5],
+    });
+
+    observer.observe(element);
+  }
+
+  intersect([e]) {
+    const nav = document.getElementById("solution-nav");
+
+    if (!nav) return;
+
+    const target = nav.querySelectorAll(`a[href='#${e.target.id}']`);
+
+    if (!target[0]) return;
+
+    target[0].setAttribute("data-active", e.isIntersecting);
   }
 }
