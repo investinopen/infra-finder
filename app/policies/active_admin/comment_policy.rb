f3f4a8328ff6ef77fc668acc18145d61aa8ae002
Alexa Grey <alexa@castironcoding.com>
2024-04-09 20:49:02 -0800

    [B] Editors can see and leave comments
    Resolves IOI-35

--- app/policies/active_admin/comment_policy.rb
+++ app/policies/active_admin/comment_policy.rb
@@ -3,20 +3,38 @@
 module ActiveAdmin
   class CommentPolicy < ApplicationPolicy
     def index?
-      super_admin?
+      has_any_admin_or_editor_access?
+    end
+
+    def show?
+      has_any_admin_or_editor_access?
     end
 
     def create?
-      true
+      has_any_admin_or_editor_access?
     end
 
     def destroy?
-      record.author == user
+      has_any_admin_access? || record.author == user
     end
 
     class Scope < ApplicationPolicy::Scope
       def resolve
-        scope.where(author: user)
+        return scope.all if has_any_admin_access?
+
+        return scope.none unless has_any_editor_access?
+
+        editable_solutions = Solution.with_editor_access_for(user).select(:id)
+        editable_drafts = SolutionDraft.where(solution_id: editable_solutions)
+
+        base = ActiveAdmin::Comment.all.unscoped
+
+        solution_comments = base.where(resource_type: "Solution", resource_id: editable_solutions)
+        draft_comments = base.where(resource_type: "SolutionDraft", resource_id: editable_drafts)
+
+        scoped = solution_comments.or(draft_comments)
+
+        scope.merge(scoped)
       end
     end
   end
