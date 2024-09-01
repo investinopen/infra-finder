345d5d88ceb853593ecfddf06ea7b51037405437
Dana Johnson <dana@castironcoding.com>
2024-08-31 21:26:17 -0700

    [E] Implement FE changes to solution comparison

---
+++ spec/components/previews/comparison_multiselection_cell_component_preview.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class ComparisonMultiselectionCellComponentPreview < ViewComponent::Preview
+  def default
+    render(ComparisonMultiselectionCellComponent.new)
+  end
+end
