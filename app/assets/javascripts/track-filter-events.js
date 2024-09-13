389aebd88a4f44a710ac4e933c0ee88eccc9e477
Dana Johnson <dana@castironcoding.com>
2024-09-13 10:29:04 -0700

    [E] Push search/filter events in bulk

--- app/assets/javascripts/track-filter-events.js
+++ app/assets/javascripts/track-filter-events.js
@@ -10,12 +10,14 @@
 export default function trackFilterEvents(event) {
   if (!(event?.target instanceof HTMLFormElement) || typeof _paq !== "object" || typeof _paq?.push !== "function") return;
 
+  const events = [];
+
   const formData = new FormData(event.target);
 
   const searchQuery = formData.get("q[name_or_provider_name_cont]");
 
   if (searchQuery) {
-    _paq.push([
+    events.push([
       "trackEvent",
       "Search/Filter Applied",
       "Search Query",
@@ -26,7 +28,7 @@ export default function trackFilterEvents(event) {
   const locationSelection = formData.get("q[country_code_eq]");
 
   if (locationSelection) {
-    _paq.push([
+    events.push([
       "trackEvent",
       "Search/Filter Applied",
       "Location",
@@ -44,7 +46,12 @@ export default function trackFilterEvents(event) {
       const eventName = el.dataset.eventName;
       const eventValue = el.dataset.eventValue;
 
-      _paq.push(["trackEvent", "Search/Filter Applied", eventName, eventValue]);
+      events.push(["trackEvent", "Search/Filter Applied", eventName, eventValue]);
     }
+  }
+
+  // push events all at once to _paq
+  if (events.length > 0) {
+    _paq.push(...events);
   }
 }
