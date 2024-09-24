829ea237adc6bb2d8440560f4b3f0837445c8256
Alexa Grey <alexa@castironcoding.com>
2024-09-24 12:20:10 -0700

    [B] Correct limit calculations for CV records

--- app/models/concerns/controlled_vocabulary_link.rb
+++ app/models/concerns/controlled_vocabulary_link.rb
@@ -18,11 +18,46 @@ module ControlledVocabularyLink
     defines :solution_kind, type: Solutions::Types::Kind
     defines :solution_association_name, type: Solutions::Types::Symbol.enum(:solution, :solution_draft)
 
+    delegate :option_association,
+      :solution_association,
+      :solution_kind,
+      :solution_link_association,
+      :solution_link_association_name,
+      :vocab_name,
+      to: :class
+
+    scope :for_assoc, ->(assoc) { where(assoc:) }
+
     scope :in_default_order, -> { joins(option_association.name).merge(option_association.klass.in_alphabetical_order) }
+    scope :multiple, -> { where(single: false) }
     scope :single, -> { where(single: true) }
   end
 
+  # @api private
+  # @return [ActiveRecord::Relation<ControlledVocabularyLink>]
+  def solution_connection_association
+    __send__(solution_association.name).__send__(solution_link_association.name)
+  end
+
+  # @api private
+  # @return [Integer]
+  def multiple_selection_count_for(assoc)
+    solution_connection_association.for_assoc(assoc).multiple.count
+  end
+
+  # @api private
+  # @return [void]
+  def check_connection_limits!
+    ControlledVocabularyConnection.each_connection_with_length_limits_for(vocab_name:, solution_kind:) do |conn|
+      if multiple_selection_count_for(conn.assoc) >= conn.max_length
+        errors.add :base, :option_limit_exceeded
+      end
+    end
+  end
+
   module ClassMethods
+    delegate :name, to: :solution_link_association, prefix: true
+
     # @param [ControlledVocabularies::Types::VocabName] name
     # @param [:actual, :draft] :kind
     # @return [void]
@@ -56,9 +91,11 @@ module ControlledVocabularyLink
     end
 
     def solution_association
-      # :nocov:
       reflect_on_association(solution_association_name)
-      # :nocov:
+    end
+
+    def solution_link_association
+      solution_association.inverse_of
     end
 
     private
@@ -80,6 +117,8 @@ module ControlledVocabularyLink
     def derive_validations!
       validates linkage.source_id, uniqueness: { scope: %i[assoc], if: :single? }
       validates linkage.source_id, uniqueness: { scope: [linkage.target_id, :assoc], unless: :single? }
+
+      before_validation :check_connection_limits!, unless: :single?
     end
   end
 end
