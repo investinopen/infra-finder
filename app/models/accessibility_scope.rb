334b5683b6c3f45c7f8f30ea4928c092ef7ed3c0
Alexa Grey <alexa@castironcoding.com>
2024-09-26 16:06:36 -0700

    [C] Migrate web accessibility applicabilities
    * Structure changed from boolean properties on the implementation
      object to a controlled vocabulary in phase 2.
    * This migrates all existing values to use the new controlled
      vocabularies and ensures they display correctly

--- app/models/accessibility_scope.rb
+++ app/models/accessibility_scope.rb
@@ -4,4 +4,12 @@ class AccessibilityScope < ApplicationRecord
   include ControlledVocabularyRecord
 
   uses_vocab! :acc_scope
+
+  def applies_to_solution?
+    provides == "applies_to_solution"
+  end
+
+  def applies_to_website?
+    provides == "applies_to_website"
+  end
 end
