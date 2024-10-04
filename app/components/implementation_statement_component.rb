e1d18fa3b596c83443de90b8d73424f6a4c238f5
Alexa Grey <alexa@castironcoding.com>
2024-10-04 14:54:50 -0700

    [C] Remove implementation statements

--- app/components/implementation_statement_component.rb
+++ app/components/implementation_statement_component.rb
@@ -2,7 +2,11 @@
 
 # The component for rendering solution implementation statements
 # within the solution show / detail component.
-
 class ImplementationStatementComponent < ApplicationComponent
   include AcceptsImplementation
+
+  def render?
+    # super && implementation.statement?
+    false
+  end
 end
