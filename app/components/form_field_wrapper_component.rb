e92a79835a2ce1678363f74be7d66b884dc8ce0f
1aurend <imlcdavidson@gmail.com>
2026-07-16 18:04:25 -0700

    feat: add conditional field functionality

--- app/components/form_field_wrapper_component.rb
+++ app/components/form_field_wrapper_component.rb
@@ -7,13 +7,32 @@ class FormFieldWrapperComponent < ApplicationComponent
   # @return [String, nil]
   attr_reader :description
 
+  # @return [{ field: Symbol, value: String, <String> }, nil]
+  attr_reader :condition
+
   # @param [String, nil] class_name
   # @param [Boolean] required
   # @param [String, nil] description
-  def initialize(class_name: nil, required: false, description: nil)
+  # @param [{ field: Symbol, value: String, <String> }, nil] condition only show this
+  #   field when the form input named `field` has the value `value` (or one of them,
+  #   when `value` is an array)
+  def initialize(class_name: nil, required: false, description: nil, condition: nil)
     @class_name = class_name
     @required = required
     @description = description
+    @condition = condition
+  end
+
+  # @return [Symbol, nil]
+  def condition_field
+    condition&.fetch(:field)
+  end
+
+  # @return [String, nil]
+  def condition_value
+    return if condition.blank?
+
+    Array(condition.fetch(:value)).join(" ")
   end
 
   # @return [Boolean]
--- app/components/form_field_wrapper_component/form_field_wrapper_component_controller.js
+++ app/components/form_field_wrapper_component/form_field_wrapper_component_controller.js
@@ -4,6 +4,16 @@ export default class extends Controller {
   static targets = ["counter"];
 
   connect() {
+    this.connectCounter();
+    this.connectCondition();
+  }
+
+  disconnect() {
+    this.field?.removeEventListener("input", this.update);
+    this.trigger?.removeEventListener("change", this.updateVisibility);
+  }
+
+  connectCounter() {
     this.field = this.element.querySelector("textarea[maxlength], input[maxlength]");
     if (!this.field || !this.hasCounterTarget) return;
 
@@ -13,11 +23,26 @@ export default class extends Controller {
     this.update();
   }
 
-  disconnect() {
-    this.field?.removeEventListener("input", this.update);
+  connectCondition() {
+    const { conditionField, conditionValue } = this.element.dataset;
+    if (!conditionField) return;
+
+    this.trigger = this.element
+      .closest("form")
+      ?.querySelector(`[name$="[${conditionField}]"], [name="${conditionField}"]`);
+    if (!this.trigger) return;
+
+    this.conditionValues = (conditionValue || "").split(" ");
+    this.updateVisibility = this.updateVisibility.bind(this);
+    this.trigger.addEventListener("change", this.updateVisibility);
+    this.updateVisibility();
   }
 
   update() {
     this.counterTarget.textContent = `${this.field.value.length}/${this.field.maxLength}`;
+  }
+
+  updateVisibility() {
+    this.element.hidden = !this.conditionValues.includes(this.trigger.value);
   }
 }
