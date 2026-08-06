94833b0b7d006509efa13c55c93edba570e64892
1aurend <imlcdavidson@gmail.com>
2026-08-06 15:05:49 -0700

    fix: update red value; switch error indicator from outline to border

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ const colors = {
   black: "hsla(0, 0%, 0%, 1)",
   white: "hsla(0, 0%, 100%, 1)",
   light: "var(--color-light)",
-  red: "hsla(12, 100%, 67%, 1)"
+  red: "hsla(342, 90%, 35%, 1)"
 };
 
 const headerProps = {
