68ad071741cccbe0bf355ac661923e39cfa51ffb
Alexa Grey <alexa@castironcoding.com>
2026-09-10 20:25:01 -0700

    chore: disable activestorage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,9 +23,6 @@ Rails.application.configure do
 
   config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"] }
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
